5e6b36f8f797462dd1789752dc92dde35c336168
aslambro1882 <0909maruf@gmail.com>
2021-09-02 11:05:19 +0600

    first commit

---
+++ app.js
@@ -0,0 +1,8 @@
+const loadData = () => {
+    const searchField = document.getElementById('search-field');
+    const searchText = searchField.value;
+    const url = `http://openlibrary.org/search.json?q=${searchText}`;
+    fetch(url)
+        .then(res => res.json())
+        .then(data => console.log(data));
+}
